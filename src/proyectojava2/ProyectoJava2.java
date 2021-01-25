3dc79b78021ec3c50060395f34e137ac8ba46639
christopher <christopher@christopher.home>
2021-01-25 19:25:05 +0100

    Mi proyecto Java

---
+++ src/proyectojava2/ProyectoJava2.java
@@ -0,0 +1,30 @@
+
+package proyectojava2;
+
+/**
+ *
+ * @author Christopher
+ */
+import java.util.Scanner;
+public class ProyectoJava2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        int n1;
+        System.out.println("Introduce un numero para ver si es par o impar: ");
+        n1 = teclado.nextInt();
+        
+        if(n1 % 2 == 0){
+            System.out.println("Es par");
+        }else{
+            System.out.println("Es impar");
+        }
+        
+        
+    }
+    
+}
